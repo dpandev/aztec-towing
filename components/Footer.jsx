554a645b75875bf2ab01968042ee48ceea80033e
David Panesiu <davidpanesiu@gmail.com>
2022-02-06 21:55:24 -0700

    Updated Footer component and styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,24 @@
+import styles from '../styles/Footer.module.css'
+import React from 'react'
+
 export const Footer = () => {
   return (
-    <div>
-      footer
+    <div className={styles.container}>
+      <div className={styles.footer}>
+        <div className={styles.footerLeft}>
+          <div className={styles.text}>
+            <span>Copyright © 2022 Aztec Towing Services, LLC.</span>
+          </div>
+          <div className={styles.text}>
+            <span>Site built by <a href='/'>Silver Crate Studios</a></span>
+          </div>
+        </div>
+        <div className={styles.footerRight}>
+          <div className={styles.contactInfo}>
+            <a href='/'>contact@example.com</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
